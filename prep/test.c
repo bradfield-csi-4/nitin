9a7eb671db978b52b83ca6fe8ee2159a1f69bd00
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2022-07-19 10:49:44 -0700

    Improvements

--- prep/test.c
+++ prep/test.c
@@ -6,51 +6,55 @@
 #include <sys/stat.h>
 
 #ifndef MAX_BUF
-#define MAX_BUF 100
+#define MAX_BUF 200
 #endif
 
 int main(void) {
 
+    DIR *dir;
+
     // Opens current directory
-    DIR *dirp = opendir(".");
-    if (dirp == NULL) {
+    if ((dir = opendir(".")) == NULL) {
         perror("opendir() error");
         exit(EXIT_FAILURE);
     }
 
-    // Stores entries in an array
+    // Stores directory entries in an array
     int i = 0;
-    struct dirent *entryp;
+    struct dirent *entry;
     struct dirent *entries[MAX_BUF];
 
-    while ((entryp = readdir(dirp)) != NULL)
-        entries[i++] = entryp;
+    while ((entry = readdir(dir)) != NULL)
+        entries[i++] = entry;
 
-    // Closes opened directory
-    closedir(dirp);
+    // Closes directory
+    closedir(dir);
 
     // Prints file info
-    printf("%-20s %-10s %-s\n\n", "Filename", "Size", "Last Modified Time");
+    printf("%-20s %-10s %-s\n\n", "Filename", "Size", "Last Modified");
 
-    struct stat sp;
+    struct stat *sp = malloc(sizeof(struct stat));
     int fd;
 
     for (int j = 0; j < i; j++) {
         char *filename = entries[j]->d_name;
 
-        // Skips hidden files/commands
+        // Skips hidden files
         if (filename[0] == '.')
             continue;
 
         fd = open(filename, O_RDONLY);
 
-        if (fst at(fd, &sp) == -1) {
+        if (fstat(fd, sp) == -1) {
             perror("fstat() error");
             exit(EXIT_FAILURE);
         }
 
-        printf("%-20s %-10lld %-ld\n", filename, sp.st_size, sp.st_mtimespec.tv_sec);
+        printf("%-20s %-10lld %-ld\n", filename, sp->st_size, sp->st_mtimespec.tv_sec);
 
         close(fd);
     }
+
+    free(sp);
 }
+
