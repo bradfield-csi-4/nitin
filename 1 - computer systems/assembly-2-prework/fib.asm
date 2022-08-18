d783ac6fdbf83d50335a2dc381c1e7457200e330
Nitin Savant <me@nitinsavant.com>
2022-08-18 16:43:43 -0400

    assembly 2 prework

--- 1 - computer systems/assembly-2-prework/fib.asm
+++ 1 - computer systems/assembly-2-prework/fib.asm
@@ -1,4 +1,19 @@
 section .text
 global fib
 fib:
+    mov eax, edi
+    cmp edi, 1
+    jle .end
+    push rbx
+    push rbp
+    lea rdi, [rdi - 1]
+    mov ebx, edi
+    call fib
+    mov rbp, rax
+    lea rdi, [rbx - 1]
+    call fib
+    add rax, rbp
+    pop rbp
+    pop rbx
+.end:
 	ret
--- 1 - computer systems/assembly-2-prework/matrix.asm
+++ 1 - computer systems/assembly-2-prework/matrix.asm
@@ -6,4 +6,9 @@ index:
 	; rdx: cols
 	; rcx: rindex
 	; r8: cindex
+	imul rdx, rcx
+    add rdx, r8
+	imul rdx, 4
+	add rdi, rdx
+	mov rax, [rdi]
 	ret
